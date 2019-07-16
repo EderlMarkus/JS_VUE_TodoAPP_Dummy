79f98bb816ce54a4225eaf88e95b5acec781f6fd
EderlMarkus <office@edma.at>
2019-07-16 17:21:54 +0200

    #2 Created Basic Setup and First components // Video: 29:32

--- vue_todolist/src/App.vue
+++ vue_todolist/src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todos v-bind:todos="todos"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Todos from './components/Todos';
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          userId: 1,
+          id: 1,
+          title: "delectus aut autem",
+          completed: false
+        },
+        {
+          userId: 1,
+          id: 2,
+          title: "quis ut nam facilis et officia qui",
+          completed: false
+        },
+        {
+          userId: 1,
+          id: 3,
+          title: "fugiat veniam minus",
+          completed: false
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
 }
 </style>
---
+++ vue_todolist/src/components/TodoItem.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="todo-item">
+        <p>{{todo.title}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TodoItem",
+    props: ["todo"]
+
+}
+</script>
+
+<style scoped>
+.todo-item {
+    background: #f4f4f4;
+    text-decoration: line-through;
+    border-bottom: 1px #ccc dotted;
+}
+
+.is-complete {
+    text-decoration: line-through;
+}
+
+.del {
+    background: #ff0000;
+    color: #fff;
+    border: none; 
+    padding: 5px, 9px; 
+    border-radius: 50%;
+    cursor: pointer; 
+    float: right;
+
+}
+</style>
+
+
