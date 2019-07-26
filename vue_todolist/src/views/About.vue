3951d7516def0cc4c2c0c371ffd59de4003e3848
EderlMarkus <office@edma.at>
2019-07-26 09:26:01 +0200

    #6 added vue-router

---
+++ vue_todolist/src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the TodList App made with the Tutorial of Traversy Media:</p>
+    <a href="https://www.youtube.com/watch?v=Wy9q22isx3U&t=3052s">https://www.youtube.com/watch?v=Wy9q22isx3U&t=3052s</a>
+  </div>
+</template>
